01afd1328f05d2f162f7039bc9d226b48dd8c0b5
HeavyMetal <dziobal294@gmail.com>
2023-01-19 20:04:34 +0100

    Add logs

--- page_object_pattern_1/pages/search_hotel.py
+++ page_object_pattern_1/pages/search_hotel.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from page_object_pattern_1.locators.locators import SearchHotelsLocators #locators in one file
+import logging
 
 class SearchHotelPage:
     def __init__(self, driver):
@@ -14,17 +15,21 @@ class SearchHotelPage:
         self.adult_input_id = "adultInput"
         self.child_input_id = "childInput"
         self.search_button_xpath = "//button[text()=' Search']"
+        self.logger = logging.getLogger(__name__)
 
     def set_city(self, city):
+        self.logger.info("Setting city {}".format(city))
         self.driver.find_element(By.XPATH, self.search_hotel_span_xpath).click()
         self.driver.find_element(By.XPATH, self.search_hotel_input_xpath).send_keys(city)
         self.driver.find_element(By.XPATH, self.location_match_xpath).click()
 
     def set_date_range(self, check_in, check_out):
+        self.logger.info("Setting check in {checkin} and check out {checkout} dates".format(checkin=check_in, checkout=check_out))
         self.driver.find_element(By.NAME, self.checkin_input_name).send_keys(check_in)
         self.driver.find_element(By.NAME, self.checkout_input_name).send_keys(check_out)
 
     def set_travellers(self, adults, child):
+        self.logger.info("Setting adults {adult} and children {children} ".format(adult=adults, children=child))
         self.driver.find_element(By.ID, self.trevelers_input_id).click()
         self.driver.find_element(By.ID, self.adult_input_id).clear()
         self.driver.find_element(By.ID, self.adult_input_id).send_keys(adults)
@@ -32,6 +37,7 @@ class SearchHotelPage:
         self.driver.find_element(By.ID, self.child_input_id).send_keys(child)
 
     def perform_search(self):
+        self.logger.info("Performing search")
         self.driver.find_element(By.XPATH, self.search_button_xpath).click()
 
 
--- page_object_pattern_1/pages/search_results.py
+++ page_object_pattern_1/pages/search_results.py
@@ -1,20 +1,27 @@
 from selenium.webdriver.common.by import By
 from page_object_pattern_1.locators.locators import SearchResultLocators #locators in one file
-
+import logging
 class SearchResultsPage:
     def __init__(self, driver):
         self.driver = driver
         self.hotel_names_xpath = "//h4[contains(@class,'list_title')]//b"
         #self.hotel_names_xpath = SearchResultLocators.hotel_names_xpath   #locators in one file/ do it for all lines
         self.hotel_prices_xpath = "//div[contains(@class,'price_tab')]//b"
+        self.logger = logging.getLogger(__name__)
     def get_hotel_names(self):
         hotels = self.driver.find_elements(By.XPATH, self.hotel_names_xpath)
-        return [hotel.get_attribute("textContent") for hotel in hotels]
+        names = [hotel.get_attribute("textContent") for hotel in hotels]
+        self.logger.info("Available hotels:")
+        for name in names:
+            self.logger.info(name)
+        return names
 
     def get_hotel_prices(self):
         prices = self.driver.find_elements(By.XPATH, self.hotel_prices_xpath)
-        return [price.get_attribute("textContent") for price in prices]
-
-
+        hotel_prices = [price.get_attribute("textContent") for price in prices]
+        self.logger.info("Hotel prices:")
+        for price in hotel_prices:
+            self.logger.info(price)
+        return hotel_prices
 
 
